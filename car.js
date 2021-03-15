50592b5e5fdf69b58fbb1872f28d1fb126c3bf70
Tdxasie <pierre.garnier1618@gmail.com>
2021-03-15 12:54:02 +0100

    added car self logginf to the cars array

--- car.js
+++ car.js
@@ -1,20 +1,29 @@
 class Car 
 {
-	constructor(_nextCar, x, _mass) 
+	constructor(_nextCar, x, _mass, _cars) 
 	{
 		this.acc = createVector(0, 0);
 		this.vel = createVector(3, 0);
-		this.pos = createVector(x, 200 - CARHEIGHT);
+		this.pos = createVector(x, ROADY - CARHEIGHT);
 		this.color = color(0);
 		this.mass = _mass;
 		this.dist = 0;
 		this.nextCar = _nextCar;
+		this.prevCar = undefined;
 		this.isSelected = false;
-		this.isFirst = false;
+		this.carsArray = _cars;
+
+		this.carsArray.unshift(this); //add this car to the first place
 
 		if (!_nextCar) {
-			this.isFirst = true
+			this.isLeader = true;
 		}
+		else
+		{
+			this.isLeader = false;
+			this.tellNextCar(); // on creating register yourself to the next car so data can be used
+		}
+
 	}
 	
 	run() 
@@ -24,12 +33,12 @@ class Car
 		this.update();
 		this.edgeCheck();
 		this.render();
-		if(!this.isFirst) this.nextCar.run()
+		if(!this.isLeader) this.nextCar.run()
 	}
 	
 	drive() 
 	{
-		if (this.isFirst)
+		if (this.isLeader)
 		{
 			this.vel.set(VMAX);
 		}
@@ -55,7 +64,7 @@ class Car
 			{
 				this.acc.set(10);
 			}
-			if(keyIsDown(32))
+			if(keyIsDown(32)) // SPACEBAR
 			{
 				this.vel.set(0);
 			}
@@ -73,29 +82,37 @@ class Car
 	
 	edgeCheck()
 	{
-		if(!this.isFirst && this.pos.x > this.nextCar.pos.x) this.pos.x = this.nextCar.pos.x - CARWIDTH;
+		if(!this.isLeader && this.pos.x + CARWIDTH > this.nextCar.pos.x) // prevent car from passing next car
+		{
+			this.pos.x = this.nextCar.pos.x - CARWIDTH;
+		}
 
-		if(this.pos.x > ROADLENGTH) {
+		if(this.pos.x > ROADLENGTH && !this.isLeader) { // if next car is out of render view, delete it 
 			delete(this.nextCar);
-			this.isFirst = true;
+			this.isLeader = true; // start behaving like the leader 
+			this.carsArray.pop(); //remove the car from the array 
 		}
 	}
 	
-	render() 
+	render() // function used for rendering the car on screen
 	{
 		stroke(0);
 		noFill();
-		text(round(this.dist), this.pos.x, this.pos.y - CARHEIGHT);
+		// text(round(this.dist), this.pos.x - CARWIDTH/2, this.pos.y - CARHEIGHT);
 		fill(this.color);
 		rect(this.pos.x, this.pos.y, CARWIDTH, CARHEIGHT);
+		// let height = map(this.dist, 0, ROADLENGTH, 100, 0, true);
+		// noStroke();
+		// fill(color('red'));
+		// ellipse(this.pos.x, this.pos.y - height, 10, 10);
 	}
 
-	clicked()
+	clicked() // detects if the car was clicked and passes on the check to the next car
 	{
 		this.color = color(0);
 		this.isSelected = false;
 		var d = dist(mouseX, mouseY, this.pos.x, this.pos.y);
-		if (d < 20)
+		if (d < CARWIDTH/2)
 		{
 			this.color = color(0, 255, 0);
 			this.isSelected = true;
@@ -103,5 +120,15 @@ class Car
 
 		this.nextCar.clicked();
 
+	}
+
+	tellNextCar()
+	{
+		this.nextCar.setPrevCar(this);
+	}
+
+	setPrevCar(car)
+	{
+		this.prevCar = car;
 	}
 }
